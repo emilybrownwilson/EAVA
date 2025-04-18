822b397744a2ac0a56a21f80ed677e31a07b790f
emilybrownwilson <ewilso28@jhu.edu>
2025-04-18 08:02:48 -0400

    Remove last Warning

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Example Data
+#'
+#' A subset of mortaility surveilance data from the Countrywide Mortality Surveillance for Action project
+#'
+#' @format `data_public` is example data frame with 10 rows and 511 columns
+#' @source <https://comsamozambique.org/data-access>
+"data_public"
--- R/odk2EAVA.R
+++ R/odk2EAVA.R
@@ -3,8 +3,6 @@
 #' @param odk A data frame which used open data kit (odk) to obtain 2016 WHO VA questionnaire responses
 #' @param id_col A unique identifier for each record within the odk data frame
 #' @returns A data frame that contains variable names and values which have been converted to openVA convention
-#' @format `data_public` is example data frame with 10 rows and 511 columns
-#' @source <https://comsamozambique.org/data-access>
 #' @references   Thomas J, Choi E, Li Z, Maire N, McCormick T, Byass P, Clark S (2021). CrossVA: Verbal Autopsy Data Transformation for InSilicoVA and InterVA5 Algorithms_. R package version 1.0.0, <https://CRAN.R-project.org/package=CrossVA>.
 #' @export
 odk2EAVA <- function(odk, id_col) {
